fbadfc85a60b72c37d56b7b5ddb37accba6f08e4
Jay Vaidya <jayvaidya96@gmail.com>
2017-03-25 19:32:04 -0500

    Added users, and tables

--- server.js
+++ server.js
@@ -1,31 +1,87 @@
-// Require the packages we will use:
+
 var http = require("http"),
-	socketio = require("socket.io"),
-	fs = require("fs");
+socketio = require("socket.io"),
+fs = require("fs");
+var express = require('express');
+var app = express();
+var photonApp = express();
 
-// Listen for HTTP connections.  This is essentially a miniature static file server that only serves our one file, client.html:
-var app = http.createServer(function(req, resp){
-	// This callback runs when a new connection is made to our HTTP server.
- 
-	fs.readFile("client.html", function(err, data){
-		// This callback runs when the client.html file has been read from the filesystem.
-
-		if(err) return resp.writeHead(500);
-		resp.writeHead(200);
-		resp.end(data);
-	});
+app.set('port', 3456);
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/noah_index.html');
 });
-app.listen(3456);
 
-// Do the Socket.IO magic:
-var io = socketio.listen(app);
+photonApp.set('port', 7777);
+photonApp.get('/unicorn/', function(req, res) {
+	res.send('1234');
+	console.log("Received GET request");
+});
+
+var server = http.createServer(app).listen(app.get('port'), function() {
+	console.log('Server is listening on port: ' + app.get('port'));
+});
+
+var photonServer = http.createServer(photonApp).listen(photonApp.get('port'), function() {
+	console.log('Server is listening on port: ' + photonApp.get('port'));
+});
+
+
+var io = socketio.listen(server);
+
+var players = [{}];
+var chatrooms = [{name: "Lobby", admin: "server", number: 0, code: 0000, started: false}];
 io.sockets.on("connection", function(socket){
-	// This callback runs when a new Socket.IO connection is established.
 
-	socket.on('message_to_server', function(data) {
-		// This callback runs when the server receives a new message from the client.
 
-		console.log("message: "+data["message"]); // log it to the Node.JS output
-		io.sockets.emit("message_to_client",{message:data["message"] }) // broadcast the message to other users
+	socket.on('add_user', function(username){
+		socket.emit('update_rooms', chatrooms);
+		// we store the username in the socket session for this client
+		socket.username = username;
+		socket.room = chatrooms[0];
+		socket.join(socket.room.name);
+		// add the client's username to the global list
+		players[0][username] = socket;
+		console.log(Object.keys(players[0]).length);
+		socket.emit('room-joined', socket.room);
+	});
+
+	socket.on('join_room', function(data){
+		var oldRoom = socket.room;
+		var newRoom = chatrooms[data.room];
+		if (newRoom.code != data.code){
+			socket.emit('error_message', "Bad password");
+		} else if (newRoom.started) {
+			socket.emit('error_message', "Game has already started");
+		} else {
+			socket.room = newRoom;
+
+			delete players[oldRoom.number][socket.username];
+			players[newRoom.number][socket.username] = socket;
+			socket.leave(oldRoom.name);
+			socket.join(socket.room.name);
+
+			// echo to client they've connected
+			socket.emit('room-joined', newRoom);
+			if (newRoom.name != "Lobby"){
+				io.sockets.in(newRoom.name).emit('update_users', { users: Object.keys(players[newRoom.number])});
+			}
+		}
+	});
+
+	socket.on('start-game', function(data){
+		console.log("Game starting " + data.name);
+		chatrooms[data.number].started = true;
+	});
+
+	socket.on('create_room', function(data){
+		var template = {name: "", admin: "", number: 0, started: false};
+		template.name = data.name;
+		template.code = data.code;
+		template.number = chatrooms.length;
+		template.admin = socket.username;
+		chatrooms.push(template);
+		players.push({});
+		io.sockets.emit('update_rooms', chatrooms);
+		socket.emit('room_created', {'room': template.number, 'code': data.code});
 	});
 });
