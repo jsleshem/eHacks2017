3d804bd651c9867266236d738f849de5c4d5cdff
Jay Vaidya <jayvaidya96@gmail.com>
2017-03-26 02:57:36 -0500

    added end

--- server.js
+++ server.js
@@ -5,16 +5,21 @@ fs = require("fs");
 var express = require('express');
 var app = express();
 var photonApp = express();
+var events = require('events');
+var eventEmitter = new events.EventEmitter();
 
 app.set('port', 3456);
 app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/noah_index.html');
 });
 
+
+
 photonApp.set('port', 7777);
 photonApp.get('/unicorn/', function(req, res) {
 	res.send('1234');
-	console.log("Received GET request");
+	console.log("Move: " + req.query.move);
+	eventEmitter.emit('get_event');
 });
 
 var server = http.createServer(app).listen(app.get('port'), function() {
@@ -29,8 +34,12 @@ var photonServer = http.createServer(photonApp).listen(photonApp.get('port'), fu
 var io = socketio.listen(server);
 
 var players = [{}];
-var chatrooms = [{name: "Lobby", admin: "server", number: 0, code: 0000, started: false, currentPlayer: ""}];
+var chatrooms = [{name: "Lobby", admin: "server", number: 0, code: 0000, started: false, currentPlayer: "", currentTurn: 0}];
 io.sockets.on("connection", function(socket){
+
+	eventEmitter.addListener("get_event", function(){
+		socket.emit("increase_score", {turn: chatrooms[socket.room.number].currentTurn, player: chatrooms[socket.room.number].currentPlayer});
+	});
 
 	socket.on('add_user', function(username){
 		socket.emit('update_rooms', chatrooms);
@@ -41,7 +50,7 @@ io.sockets.on("connection", function(socket){
 		// add the client's username to the global list
 		players[0][username] = socket;
 		console.log(Object.keys(players[0]).length);
-		socket.emit('room-joined', socket.room);
+		socket.emit('room_joined', socket.room);
 	});
 
 	socket.on('join_room', function(data){
@@ -60,16 +69,18 @@ io.sockets.on("connection", function(socket){
 			socket.join(socket.room.name);
 
 			// echo to client they've connected
-			socket.emit('room-joined', newRoom);
+			socket.emit('room_joined', newRoom);
 			if (newRoom.name != "Lobby"){
 				io.sockets.in(newRoom.name).emit('update_users', { users: Object.keys(players[newRoom.number])});
 			}
 		}
 	});
 
-	socket.on('start-game', function(data){
+	socket.on('start_game', function(data){
 		console.log("Game starting " + data.name);
 		chatrooms[data.number].started = true;
+		chatrooms[data.number].currentTurn = 17;
+		io.sockets.in(data.name).emit("start_turn", chatrooms[data.number].currentPlayer);
 	});
 
 	socket.on('create_room', function(data){
@@ -78,10 +89,36 @@ io.sockets.on("connection", function(socket){
 		template.code = data.code;
 		template.number = chatrooms.length;
 		template.admin = socket.username;
-        template.currentPlayer = socket.username;
+    template.currentPlayer = socket.username;
 		chatrooms.push(template);
 		players.push({});
 		io.sockets.emit('update_rooms', chatrooms);
 		socket.emit('room_created', {'room': template.number, 'code': data.code});
 	});
+
+	socket.on('end_turn', function(data){
+
+		var players_names = Object.keys(players[socket.room.number]);
+		console.log(players_names);
+		var currentIndex = 0;
+		for (var i in players_names){
+			console.log(i);
+			if (players_names[i] == socket.username){
+				currentIndex = parseInt(i);
+			}
+		}
+		if (currentIndex+1 >= players_names.length){
+			if (chatrooms[socket.room.number].currentTurn == 18) {
+				io.sockets.in(data.name).emit("end_game", players_names);
+			} else {}
+			chatrooms[socket.room.number].currentTurn = parseInt(chatrooms[socket.room.number].currentTurn) + 1;
+			chatrooms[socket.room.number].currentPlayer = players_names[0];
+			io.sockets.in(data.name).emit("start_turn", players_names[0]);
+		}
+		} else {
+			chatrooms[socket.room.number].currentPlayer = players_names[currentIndex+1];
+			io.sockets.in(data.name).emit("start_turn", players_names[currentIndex+1]);
+		}
+	});
+
 });
